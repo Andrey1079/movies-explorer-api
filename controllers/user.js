f94de2120b28a7425f071648fd648f4ea2dc6c3f
Andrey Dunaev <dunayev@yandex.ru>
2023-09-14 10:54:43 +0700

    feat настроить контроллер изменения данных пользователя

--- controllers/user.js
+++ controllers/user.js
@@ -19,17 +19,23 @@ module.exports.getMyInfo = (req, res, next) => {
     .then((user) => {
       res.send(user);
     })
-    .catch((err) => {
-      if (err.name === 'CastError') {
-        next(new BadRequest(badRequestMessages.idIsWrong));
-      }
-      next(err);
-    });
+    .catch(next);
 };
 
 module.exports.changeMyInfo = (req, res, next) => {
-  console.log(req.user);
-  // User.findByIdAndUpdate(req.user._id, req.body``);
+  const { _id } = req.user;
+  if (req.body.name) req.body.name = escape(req.body.name);
+  User.findByIdAndUpdate(_id, req.body, { new: true, runValidators: true })
+    .orFail(new NotFound(notfoundMessages.userIsAbsent))
+    .then((user) => {
+      res.send(user);
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        next(new BadRequest(err.message));
+      }
+      next(err);
+    });
 };
 
 module.exports.signIn = (req, res, next) => {
